083bf34f565b0406fc8e31eba3eeba6bcc620cf7
bashyrov <bashyroov@gmail.com>
2025-03-21 10:27:21 +0100

    Fix flake8

--- taxi/models.py
+++ taxi/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class Manufacturer(models.Model):
     name = models.CharField(max_length=100, unique=True)
     country = models.CharField(max_length=100)
@@ -10,16 +11,26 @@ class Manufacturer(models.Model):
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=100, unique=True)
+    license_number = models.CharField(
+        max_length=100,
+        unique=True)
 
     def __str__(self):
-        return f"{self.username} - {self.first_name} {self.last_name}: {self.license_number}"
+        return (f"{self.username} - "
+                f"{self.first_name} "
+                f"{self.last_name}: "
+                f"{self.license_number}")
 
 
 class Car(models.Model):
     model = models.CharField(max_length=100)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='manufactured_cars')
-    drivers = models.ManyToManyField(Driver, related_name='driven_cars')
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name='manufactured_cars')
+    drivers = models.ManyToManyField(
+        Driver,
+        related_name='driven_cars')
 
     def __str__(self):
         return self.model
